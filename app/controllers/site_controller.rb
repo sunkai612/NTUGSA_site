8c970ad2dba683eead751a04d019b24d4fd3a4dd
Kai Sun <tonysun329@gmail.com>
2014-09-09 23:47:27 +0800

    finish homepage UI adjustment

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,8 @@ class SiteController < ApplicationController
     @rotator = Rotator.all#.order("imageOrder ASC")
     @post_newest = Post.order("id DESC").limit(10)
     @post_mostCommented = Post.where("created_at > ?", Time.now.months_ago(1)).order("comments_count DESC").limit(10)
+    @new_event = Event.order("id DESC").limit(3)
+    @new_record = Record.order("id DESC").limit(1)
   end
 
   def about 
