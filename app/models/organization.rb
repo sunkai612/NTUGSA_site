65e874fb1f95c7896d36f57b5286dd7a52f7ffd1
Juin Chiu <davidjuin0519@gmail.com>
2014-09-12 17:48:03 +0800

    fix backstage organization & member

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ActiveRecord::Base
   has_many :members, :dependent => :destroy 
   has_many :records, :dependent => :destroy 
+  validates :title, :presence => true
+  validates :content, :presence => true
 end
