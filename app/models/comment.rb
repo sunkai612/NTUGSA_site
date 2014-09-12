220ccc3f3b7155376f784559f0566c3cfc9f64c7
Juin Chiu <davidjuin0519@gmail.com>
2014-09-12 14:41:32 +0800

    fix template break of board & post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 
-	validates :commenter, :presence => true
-	validates :content, :presence => true
+	validates :commenter, :presence => true, length: { in: 2..15 }
+	validates :content, :presence => true, length: {maximum: 50}
 
 	belongs_to :post, counter_cache: :comments_count
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
 	validates :author, :presence => true, length: { in: 2..15 }
 	validates :type_name, :presence => true, length: { is: 2 }
-	validates :title, :presence => true, length: { in: 4..60 }
+	validates :title, :presence => true, length: { in: 4..20 }
 	validates :content, :presence => true, length: { minimum: 20 }
 
 	belongs_to :board, counter_cache: :posts_count
