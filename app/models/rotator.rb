1d4518edb52b02cceca500cfa6be656947c7b103
Juin Chiu <davidjuin0519@gmail.com>
2014-09-29 19:54:14 +0800

    fix member order

--- app/models/rotator.rb
+++ app/models/rotator.rb
@@ -1,7 +1,7 @@
 class Rotator < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  validates :avatar, :attachment_presence => true
+#  validates :avatar, :attachment_presence => true
   validates :title, :presence => true
   validates :link, :presence => true
 end
